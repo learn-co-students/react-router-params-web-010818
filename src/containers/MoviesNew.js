5fc4d9111476c6dd3527112d641838ec2d6d08a6
cnlucke <cristy.lucke@gmail.com>
2018-03-31 19:35:39 -0400

    Done.

--- src/containers/MoviesNew.js
+++ src/containers/MoviesNew.js
@@ -14,7 +14,13 @@ class MoviesNew extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    this.props.addMovie(this.state);
+    // Destructure addMovie and history from the components props
+    const { addMovie, history } = this.props;
+    // Create the movie with the Redux action
+    addMovie(this.state);
+    this.setState({title: ''})
+    // redirect to /movies route
+    history.push('/movies')
   }
 
   handleOnChange = event => {
@@ -26,9 +32,10 @@ class MoviesNew extends Component {
   render(){
     return (
       <form style={{ marginTop: '16px' }} onSubmit={this.handleOnSubmit} >
-        <input 
-          type="text" 
-          onChange={this.handleOnChange} 
+        <input
+          type="text"
+          onChange={this.handleOnChange}
+          value={this.state.title}
           placeholder="Add a Movie" />
         <input type="submit" value="Add Movie" />
       </form>
